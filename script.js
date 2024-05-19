6971faf4df5c51ec3d71bbb4acb14dcce884be2a
Goromatic <infocherrycollins@gmail.com>
2024-05-19 20:42:49 -0300

    tarea 11

---
+++ script.js
@@ -0,0 +1,17 @@
+let nombre="pedrito";
+let apellido="Suarez";
+let age=25;
+console.log(age +" "+nombre)
+
+let numberone=120;
+let numbertwo=20;
+let resultsuma = numberone + numbertwo;
+let resultresta = numberone - numbertwo;
+let resultmulti = numberone * numbertwo;
+let resultdivision = numberone / numbertwo;
+console.log("suma:",resultsuma);
+console.log("resta:",resultresta);
+console.log("multiplicacion:",resultmulti);
+console.log("division:",resultdivision);
+
+
