0d13d38332e281bbce59f0a9229a72a1109a6476
Goromatic <infocherrycollins@gmail.com>
2024-05-22 21:00:42 -0300

    tarea 12

--- script.js
+++ script.js
@@ -36,51 +36,99 @@
 // console.log(nombre[i])
 // }
 
-let topping = "oreo";
-let precio = 0;
-let preciofinal = 0;
-let helado = 350;
+// let topping = "oreo";
+// let precio = 0;
+// let preciofinal = 0;
+// let helado = 350;
 
-if(topping=="oreo"){
-    precio= 10;
-} else if(topping =="kitkat"){
-    precio=15;
-} else if(topping=="kinder"){
-    precio= 25;
-} else{
-    console.log("no tenemos este topping");
-}
+// if(topping=="oreo"){
+//     precio= 10;
+// } else if(topping =="kitkat"){
+//     precio=15;
+// } else if(topping=="kinder"){
+//     precio= 25;
+// } else{
+//     console.log("no tenemos este topping");
+// }
 
-preciofinal = helado + precio;
-console.log ("el helado cuesta $"+preciofinal);
+// preciofinal = helado + precio;
+// console.log ("el helado cuesta $"+preciofinal);
 
-let menu = "pescado"
-switch (menu) {
-    case"pescado":
-    console.log ("vino blanco");
-    break;
-    case "carne":
-        console.log ("vino tinto");
-    case "verduras":
-        console.log ("agua");
-    break;
-    default:
-        console.log ("elija entre carne,verdura o pesado");
+// let menu = "pescado"
+// switch (menu) {
+//     case"pescado":
+//     console.log ("vino blanco");
+//     break;
+//     case "carne":
+//         console.log ("vino tinto");
+//     case "verduras":
+//         console.log ("agua");
+//     break;
+//     default:
+//         console.log ("elija entre carne,verdura o pesado");
 
-}
+// }
 
-let names=["britney","justin","nick","brian"];
-console.log (names);
-console.log(names[0]);
-console.log(names[2]);
-names.push("robertito");
-
-let nota = 9;
- if (nota <11) {
-     console.log("el numero es mas chico que 11");
- }
+// let names=["britney","justin","nick","brian"];
+// console.log (names);
+// console.log(names[0]);
+// console.log(names[2]);
+// names.push("robertito");
 
 
+
+
+
+// let nota = 9;
+//  if (nota <11) {
+//      console.log("el numero es mas chico que 11");
+//  }
+
+// let num=1;
+// let sum=0;
+// while (num<=10){
+//     sum+= num;
+//     num++;
+// }
+// console.log("La suma de los números del 1 al 10 es: " + sum);
+
+
+// //let resultado;
+// let numero1 = 3;
+// let numero2 = 5;
+// function sumashow(){
+//     resultado= numero1+numero2;
+// alert("el resultado es "+resultado)
+// }
+// //sumashow();
+
+// var numero = prompt("Introduce un número entero");
+
+// let resultado = parImpar(numero);
+
+// alert("El número " + numero + " es " + resultado);
+
+// function parImpar(numero) {
+//   if (numero % 2 == 0) {
+//     return "par";
+//   } else {
+//     return "impar";
+//   }
+// }
+
+let  nombre = prompt ("ingrese su nombre");
+function saludo (){
+    if (nombre){
+        alert ("bienvenido " + nombre)
+    }
+        else{
+            alert ("ingrese su nombre")
+        }
+
+
+    }
+
+saludo();
 
 
 
